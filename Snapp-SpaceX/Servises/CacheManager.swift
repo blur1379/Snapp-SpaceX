bbb7756ab0f2b44ec6379ad894ebee637000a82c
Mehdi Karami <karami.mehdi.work@github.com>
2023-11-29 22:54:11 +0330

    Add documentation for actor patterns in Network and CacheManager

--- Snapp-SpaceX/Servises/CacheManager.swift
+++ Snapp-SpaceX/Servises/CacheManager.swift
@@ -4,12 +4,17 @@
 //
 //  Created by Mohammad Blur on 8/10/23.
 //
+
 import SwiftUI
 
+// MARK: - Cache Manager
+/// An actor that manages caching of UIImage objects.
 actor CacheManager {
+    // Shared instance of the cache manager for convenient access.
     static let shared = CacheManager()
     private init() {}
     
+    // NSCache instance used for caching UIImage objects.
     private var imageCache: NSCache<NSString, UIImage> = {
         let cache = NSCache<NSString, UIImage>()
         cache.countLimit = 100
@@ -17,14 +22,26 @@ actor CacheManager {
         return cache
     }()
     
+    /// Adds an image to the cache with a specified name.
+    ///
+    /// - Parameters:
+    ///   - image: The UIImage to be cached.
+    ///   - name: The unique identifier for the image in the cache.
     func add(image: UIImage, name: String) {
         imageCache.setObject(image, forKey: name as NSString)
     }
     
+    /// Removes an image from the cache with a specified name.
+    ///
+    /// - Parameter name: The unique identifier for the image in the cache.
     func remove(name: String) {
         imageCache.removeObject(forKey: name as NSString)
     }
     
+    /// Retrieves an image from the cache with a specified name.
+    ///
+    /// - Parameter name: The unique identifier for the image in the cache.
+    /// - Returns: The cached UIImage, or `nil` if not found.
     func get(name: String) -> UIImage? {
         imageCache.object(forKey: name as NSString)
     }
